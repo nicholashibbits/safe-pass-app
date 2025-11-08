8ffe65ea83d036c51be5209e4cf9c2407b20ef09
Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>
2025-11-07 20:47:52 -0600

    create components for new entry

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get '/about', to: 'pages#about'
   get "up" => "rails/health#show", as: :rails_health_check
 
+  resources :entries
+
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
