eb5ba0c2df3752c865f8fedcb06c33a2e9b44e7a
Nick Hibbits <172406954+nicholashibbits1@users.noreply.github.com>
2025-11-07 20:51:24 -0600

    add create method to entries controller

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,6 +1,23 @@
 class EntriesController < ApplicationController
+  before_action :authenticate_user!
 
   def new
     @entry = Entry.new
   end 
+
+  def create 
+    @entry = current_user.entries.new(entry_params)
+    if @entry.save
+      flash[:notice] = 'Entry saved'
+      redirect_to root_path
+    else
+      flash[:alert] = 'Sorry, there was an issue'
+      render :new, status: unprocessable_entity
+    end
+  end
+
+  private
+  def entry_params
+    params.expect(entry: [:name, :url, :username, :password])
+  end
 end
